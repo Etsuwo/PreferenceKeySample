421928fb69b6267c5d81eca7453d459a1895d56e
Etsushi <etsushi.otani@emoshu.co.jp>
2022-04-10 23:17:32 +0900

    Initial Commit

---
+++ PreferencekeySample/PreferencekeySampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreferencekeySampleApp.swift
+//  PreferencekeySample
+//
+//  Created by Etsushi Otani on 2022/04/10.
+//
+
+import SwiftUI
+
+@main
+struct PreferencekeySampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
