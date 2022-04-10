d7d6e3ea5197d14a6e16bbafdfaf5f9f0c15bbf9
Etsushi <etsushi.otani@emoshu.co.jp>
2022-04-10 23:21:31 +0900

    [add] SubViewファイル作成

---
+++ PreferencekeySample/SubView.swift
@@ -0,0 +1,20 @@
+//
+//  SubView.swift
+//  PreferencekeySample
+//
+//  Created by Etsushi Otani on 2022/04/10.
+//
+
+import SwiftUI
+
+struct SubView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SubView_Previews: PreviewProvider {
+    static var previews: some View {
+        SubView()
+    }
+}
